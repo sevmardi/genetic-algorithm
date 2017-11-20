72af9f373eef81bbcb3265b2ec803eadf5a87bb1
Sevak Mardirosian <smardirosians@gmail.com>
2017-11-20 15:41:27 +0100

    added few things.

--- save_to_csv.m
+++ save_to_csv.m
@@ -1,8 +1,13 @@
-function save_to_csv(bits, budget)
- for i=bits:budget
+function save_to_csv(bits)
+ budget = 5000 * bits;
+ x = [];
+ for i= 1:20:
     P = 5;
-    filename = sprintf('run%d.csv',i);
-    csvwrite(filename, P);
+	ga(bits, budget)
+	x = [x; labs(ans)]
+    // filename = sprintf('run%d.csv',i);
+    // csvwrite(filename, P);
+    csvwrite("test.txt",x)
  end
 end
 
